8485ee71488a222b6b062e42a04a6601a28d0b31
Felipe Polo <felipe.polo@gmail.com>
2020-03-27 14:13:32 +0100

    Being able to generate pdfs

--- bin/pdf/generate-cv.js
+++ bin/pdf/generate-cv.js
@@ -1,12 +1,15 @@
 const config = require('../../config');
-const initBamboo = require('./bamboo');
+const initBamboo = require('../../lib/bamboo');
+const initPdf = require('../../lib/pdf');
 
 const bamboo = initBamboo(config.bamboo.token);
+const pdf = initPdf();
 
 (async () => {
     const [ , , email] = process.argv;
     const { employees } = await bamboo.getStaff();
     const { id } = employees.find(({ workEmail }) => workEmail === email);
     const target = await bamboo.getEmployee(id);
+    await pdf.generate();
     console.log(target);
 })();
