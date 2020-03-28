367b26b2f41d097a7bd062bfe9576b26c396f521
Felipe Polo <felipe.polo@gmail.com>
2020-03-28 18:43:22 +0100

    POC to generate chart picture

--- bin/pdf/generate-cv.js
+++ bin/pdf/generate-cv.js
@@ -3,12 +3,16 @@ const path = require('path');
 const config = require('../../config');
 const initBamboo = require('../../lib/bamboo');
 const initPdf = require('../../lib/pdf');
+const initChart = require('../../lib/chart');
 
 const templatePath = path.resolve(__dirname, 'pdf-template.pug');
 const stylePath = path.resolve(__dirname, 'pdf-template.scss');
 
 const bamboo = initBamboo(config.bamboo.token);
 const pdf = initPdf(templatePath, stylePath);
+const chart = initChart('radar', {
+
+});
 
 const storePicture = async (picPath, picture) => {
     // better refresh in S3 or similar
@@ -19,6 +23,13 @@ const generatePicturePath = id => path.resolve(__dirname, '..', '..', 'pics', `$
 const getPictureLink = id => `../../pics/${id}.jpg`;
 
 (async () => {
+    const data = {
+        labels: ['Running', 'Swimming', 'Eating', 'Cycling', 'A', 'B'],
+        datasets: [{
+            data: [20, 10, 4, 2, 5, 12]
+        }]
+    };
+    await chart.generate(1)(data);
     const [ , , email] = process.argv;
     const { employees } = await bamboo.getStaff();
     const { id } = employees.find(({ workEmail }) => workEmail === email);
@@ -26,9 +37,8 @@ const getPictureLink = id => `../../pics/${id}.jpg`;
         bamboo.getEmployee(id),
         await bamboo.getPicture('medium')(id),
     ]);
-    const picPath = generatePicturePath(email);
+    const picPath = generatePicturePath(id);
     await storePicture(picPath, picture);
-    console.log(getPictureLink(id));
     const file = await pdf.generate({
         ...target,
         // picture: "http://github.com/BuckyMaler/me/raw/master/assets/img/avatar.jpg"
